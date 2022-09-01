ccec6a68c1d0bb2341d0a4f2280be76bfdf68341
Jannes Kaspar-Müller <jannes.kaspar-mueller@co-it.eu>
2022-09-01 21:44:19 +0200

    Added constructor for credentials

--- Clockodo.NET/DataContracts/Credentials.cs
+++ Clockodo.NET/DataContracts/Credentials.cs
@@ -2,6 +2,14 @@
 
 public class Credentials
 {
+    public Credentials(string emailAddress, string apiToken, string applicationName, string contactEmail)
+    {
+        EmailAddress = emailAddress;
+        ApiToken = apiToken;
+        ApplicationName = applicationName;
+        ContactEmail = contactEmail;
+    }
+
     public string EmailAddress { get; set; }
     public string ApiToken { get; set; }
     public string ApplicationName { get; set; }
